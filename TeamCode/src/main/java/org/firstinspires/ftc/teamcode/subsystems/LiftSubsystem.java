0062c148a1e079a54da7463f06d70db7cb03f2c5
saltyeg <31107399+saltyeg@users.noreply.github.com>
2026-03-16 17:47:35 -0700

    Update LiftSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftSubsystem.java
@@ -3,13 +3,16 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
+import com.seattlesolvers.solverslib.hardware.motors.MotorGroup;
 
 public class LiftSubsystem extends SubsystemBase {
     private MotorEx liftLeft;
     private MotorEx liftRight;
+    private MotorGroup liftMotorGroup;
     public LiftSubsystem(final HardwareMap hardwareMap) {
         liftLeft = hardwareMap.get(MotorEx.class, "liftLeft");
         liftRight = hardwareMap.get(MotorEx.class, "liftRight");
+        liftMotorGroup = new MotorGroup(liftLeft, liftRight);
     }
     public void up() {
         //set pid target to up
