be2faac95feae752b576272f1a7edb60959f20d9
saltyeg <31107399+saltyeg@users.noreply.github.com>
2026-03-16 17:53:44 -0700

    fix lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/global/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/global/Robot.java
@@ -1,8 +1,28 @@
 package org.firstinspires.ftc.teamcode.global;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
+import com.seattlesolvers.solverslib.hardware.motors.MotorGroup;
+
 public class Robot extends com.seattlesolvers.solverslib.command.Robot {
     private static final Robot instance = new Robot();
+
+    private MotorEx liftLeft;
+    private MotorEx liftRight;
+    private MotorGroup liftMotorGroup;
+
+    public void init(final HardwareMap hm) {
+        liftLeft = hm.get(MotorEx.class, "liftLeft");
+        liftRight = hm.get(MotorEx.class, "liftRight");
+        liftMotorGroup = new MotorGroup(liftLeft, liftRight);
+
+
+    }
+
     public static Robot getInstance() {
         return instance;
     }
+
+
 }
